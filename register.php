882898fcd2fcb1fc718b370019643440a614ad92
Kevin Rivaldo Panggabean <82790760+krpauto@users.noreply.github.com>
2024-09-25 13:05:35 +0700

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Buat Akun Baru</title>
+  <title>Create Account</title>
 
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
